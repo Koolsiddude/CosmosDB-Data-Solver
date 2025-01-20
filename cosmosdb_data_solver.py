a0903b7ad53fc8417d9160a47d06f2d1ccfc215c
Prabhu <siddhartha.prabhu@noventiq.com>
2025-01-20 23:21:09 +0530

    Corrections Commit 2

--- cosmosdb_data_solver.py
+++ cosmosdb_data_solver.py
@@ -3,10 +3,10 @@ import streamlit as st
 # Apply wide mode only for the first page
 st.set_page_config(page_title="CosmosDB Data Uploader", page_icon=":rocket:", layout="wide")
 
-mainpage = st.Page("content/home/CosmosDB-FileUploader.py", title="Main Page", default=True)
-cosmosDBsetup = st.Page("content/settings/CosmosDB-Setup.py", title="CosmosDB Setup Page")
-azureOpenAIsetup = st.Page("content/settings/AzureOpenAI-Setup.py", title="Azure OpenAI Setup Page")
-fileUploader = st.Page("content/upload/File-Uploader.py", title="File Uploader Page")
+mainpage = st.Page("home/CosmosDB-FileUploader.py", title="Main Page", default=True)
+cosmosDBsetup = st.Page("settings/CosmosDB-Setup.py", title="CosmosDB Setup Page")
+azureOpenAIsetup = st.Page("settings/AzureOpenAI-Setup.py", title="Azure OpenAI Setup Page")
+fileUploader = st.Page("upload/File-Uploader.py", title="File Uploader Page")
 
 pg = st.navigation(
     {
